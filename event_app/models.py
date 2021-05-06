d06f90f86cfad3f3adc7879506152405f1415b94
fyriyadi <fajar.yan.riyadi@gmail.com>
2021-05-06 12:06:58 +0700

    Event,Trainer,Facility Models, add models to admin pannel

---
+++ event_app/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+import multiselectfield
+
+   
+
+class Event(models.Model):
+    event_name = models.CharField(max_length=200, null=True)
+    categories = (('Webinar','webinar'), ('Seminar','seminar'), ('Workshop','workshop'))
+    event_category = models.CharField(max_length=20, choices=categories, null=True, default='Workshop')
+    audiences = (('Umum','umum'), ('Guru','guru'))
+    event_audience = models.CharField(max_length=20, choices=audiences, null=True, default='Umum')
+    ticket_price = models.IntegerField(null=True, default=0)
+    ticket_available = models.IntegerField(null=True, default=0)
+    event_summary = models.CharField(max_length=100, null=True)
+    event_date = models.DateField(null=True, blank=True,default='2021-05-19')
+    event_start_time = models.TimeField(null=True, blank=True, default='08:30')
+    event_finish_time = models.TimeField(null=True, blank=True, default='14:30')
+    event_facilities = models.ManyToManyField('Facility', blank=True)
+    event_venue = models.CharField(max_length=100, null=True)
+    event_trainer = models.ManyToManyField('Trainer', blank=True)
+    event_thumbnail = models.ImageField(null=True, blank=True)
+    event_image_header = models.ImageField(null=True, blank=True)
+    event_detail = models.CharField(max_length=500, null=True)
+    event_contact_person = models.CharField(max_length=200, null=True)    
+    
+    def __str__(self):
+        return self.event_name
+
+
+class Facility(models.Model):
+	facility_name = models.CharField(max_length=100, null=True)
+
+	def __str__(self):
+		
+		return self.facility_name
+
+
+
+class Trainer(models.Model):
+	trainer_name = models.CharField(max_length=100, null=True)
+	trainer_photo = models.ImageField(upload_to='static/images/trainer_photos/', null=True, blank=True)
+	trainer_title = models.CharField(max_length=100, null=True)
+	trainer_twitter = models.CharField(max_length=200, blank=True, null=True)
+	trainer_linkedin = models.CharField(max_length=200, blank=True, null=True)
+	
+
+	def __str__(self):
+		return self.trainer_name
+
+	@property
+	def imageURL(self):
+		try:
+			url = self.trainer_photo.url
+		except:
+			url = ''
+		return url
+
--- eventproject/settings.py
+++ eventproject/settings.py
@@ -44,12 +44,16 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'account_app',
+    'event_app',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'multiselectfield',
 ]
 
 MIDDLEWARE = [
